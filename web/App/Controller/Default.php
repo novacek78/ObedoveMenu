fed6bb993b8c9280468abe6db5fe0e1fa396d4de
Enriqe <info@brevistener.com>
2014-12-11 00:25:00 +0100

    Novy font

--- web/App/Controller/Default.php
+++ web/App/Controller/Default.php
@@ -22,54 +22,6 @@ class App_Controller_Default extends MFW_Controller {
                 Počas štartu projektu je to zatiaľ zdarma. Vytváranie denného / týždenného obedového menu nebolo nikdy jednoduchšie.
                 Podrobnejšie informácie tu &gt;'
             ],
-            [
-                'nadpis' => 'Pre zákazníkov',
-                'text'   => 'Ak Vás už omrzelo každý deň zisťovať, čo varia vo Vašej obľúbenej reštaurácii, ste na správnom mieste.
-                Prihláste sa na zasielanie obedového menu z Vašej reštaurácie priamo na Váš e-mail, aby ste tak mali dennú ponuku vždy po ruke.
-                Viac informácií nájdete tu &gt;'
-            ],
-            [
-                'nadpis' => 'Pre reštaurácie',
-                'text'   => 'Ak Vás už omrzelo každý deň vymýšľať obedové menu a potom ho písať vo Worde, zaregistrujte sa hneď teraz!
-                Počas štartu projektu je to zatiaľ zdarma. Vytváranie denného / týždenného obedového menu nebolo nikdy jednoduchšie.
-                Podrobnejšie informácie tu &gt;'
-            ],
-            [
-                'nadpis' => 'Pre zákazníkov',
-                'text'   => 'Ak Vás už omrzelo každý deň zisťovať, čo varia vo Vašej obľúbenej reštaurácii, ste na správnom mieste.
-                Prihláste sa na zasielanie obedového menu z Vašej reštaurácie priamo na Váš e-mail, aby ste tak mali dennú ponuku vždy po ruke.
-                Viac informácií nájdete tu &gt;'
-            ],
-            [
-                'nadpis' => 'Pre reštaurácie',
-                'text'   => 'Ak Vás už omrzelo každý deň vymýšľať obedové menu a potom ho písať vo Worde, zaregistrujte sa hneď teraz!
-                Počas štartu projektu je to zatiaľ zdarma. Vytváranie denného / týždenného obedového menu nebolo nikdy jednoduchšie.
-                Podrobnejšie informácie tu &gt;'
-            ],
-            [
-                'nadpis' => 'Pre zákazníkov',
-                'text'   => 'Ak Vás už omrzelo každý deň zisťovať, čo varia vo Vašej obľúbenej reštaurácii, ste na správnom mieste.
-                Prihláste sa na zasielanie obedového menu z Vašej reštaurácie priamo na Váš e-mail, aby ste tak mali dennú ponuku vždy po ruke.
-                Viac informácií nájdete tu &gt;'
-            ],
-            [
-                'nadpis' => 'Pre reštaurácie',
-                'text'   => 'Ak Vás už omrzelo každý deň vymýšľať obedové menu a potom ho písať vo Worde, zaregistrujte sa hneď teraz!
-                Počas štartu projektu je to zatiaľ zdarma. Vytváranie denného / týždenného obedového menu nebolo nikdy jednoduchšie.
-                Podrobnejšie informácie tu &gt;'
-            ],
-            [
-                'nadpis' => 'Pre zákazníkov',
-                'text'   => 'Ak Vás už omrzelo každý deň zisťovať, čo varia vo Vašej obľúbenej reštaurácii, ste na správnom mieste.
-                Prihláste sa na zasielanie obedového menu z Vašej reštaurácie priamo na Váš e-mail, aby ste tak mali dennú ponuku vždy po ruke.
-                Viac informácií nájdete tu &gt;'
-            ],
-            [
-                'nadpis' => 'Pre reštaurácie',
-                'text'   => 'Ak Vás už omrzelo každý deň vymýšľať obedové menu a potom ho písať vo Worde, zaregistrujte sa hneď teraz!
-                Počas štartu projektu je to zatiaľ zdarma. Vytváranie denného / týždenného obedového menu nebolo nikdy jednoduchšie.
-                Podrobnejšie informácie tu &gt;'
-            ],
         ];
 
         $V->render();
